15680d46555a2ca6f6480133859e07ffce8b3b3d
emtalen <59577366+emtalen@users.noreply.github.com>
2020-09-14 14:12:01 +0200

    scaffolding react app, adding a simple header and installing cypress

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <section data-cy='header'>
+        <h1>Employee List</h1>
+      </section>
+    </>
+  );
+};
+
+export default App;
